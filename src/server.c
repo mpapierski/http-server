66da3bf0bca3e5bbf65aa4a309d88d818c3d003d
Michał Papierski <michal@papierski.net>
2015-01-01 16:13:22 +0100

    Free response after it was sent completely
    This change allows for multiple responses to be sent using
    the same connection.

--- src/server.c
+++ src/server.c
@@ -568,6 +568,11 @@ int http_server_socket_action(http_server * srv, http_server_socket_t socket, in
         {
             return HTTP_SERVER_SOCKET_ERROR;
         }
+        // All response data is sent now. No need to hold this memory,
+        // and allow next requests inside the same connection to create
+        // new responses.
+        http_server_response_free(res);
+        it->current_response_ = NULL;
         // All response is sent. Poll again for new request
         if (http_server_poll_client(it, HTTP_SERVER_POLL_IN) != HTTP_SERVER_OK)
         {
